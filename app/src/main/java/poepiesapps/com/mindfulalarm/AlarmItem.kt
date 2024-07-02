78933da34338f37351ff6508ae4daf8058611968
TristenMarto <tristen.marto@gmail.com>
2024-07-02 18:04:23 +0200

    Allow multiple alarms

---
+++ app/src/main/java/poepiesapps/com/mindfulalarm/AlarmItem.kt
@@ -0,0 +1,9 @@
+package poepiesapps.com.mindfulalarm
+
+data class AlarmItem (
+    val name: String,
+    val startHour: Int,
+    val startMinute: Int,
+    val endHour: Int,
+    val endMinute: Int,
+)
